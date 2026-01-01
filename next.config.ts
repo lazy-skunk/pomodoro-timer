dc3e366de4dbac2cf53ca4277484c79b1ebe5cb0
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2026-01-01 05:54:01 +0000

    chore(config): remove unused setting

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  watchOptions: {
-    pollIntervalMs: 2000,
-  },
 };
 
 export default nextConfig;
