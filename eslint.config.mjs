07344f33be50a42491db9d815080aa51e9b759ef
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2026-05-06 14:57:48 +0000

    chore: minor modification

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import prettier from "eslint-config-prettier";
-import { defineConfig, globalIgnores } from "eslint/config";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -11,21 +10,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = defineConfig([
+const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   prettier,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "coverage/**",
-    "next-env.d.ts",
-    "src/types/cache-life.d.ts",
-    "src/types/routes.d.ts",
-    "src/types/validator.ts",
-  ]),
-]);
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "coverage/**",
+      "next-env.d.ts",
+      "src/types/cache-life.d.ts",
+      "src/types/routes.d.ts",
+      "src/types/validator.ts",
+    ],
+  },
+];
 
 export default eslintConfig;
