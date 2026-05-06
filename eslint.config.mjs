d0090257a8a63de03a0d6ba663c59aff0592a952
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2026-05-06 09:25:38 +0000

    fix: use flatcompat for next.js eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
 import prettier from "eslint-config-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
